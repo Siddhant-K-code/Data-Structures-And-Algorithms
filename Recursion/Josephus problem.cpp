710ea2a7c6283933ff3a0831c25a83059637c7ff
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-09-12 22:24:24 +0530

    Create Josephus problem.cpp

---
+++ Recursion/Josephus problem.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int jos(long long int n, long long int k)
+{
+    if (n == 1)
+        return n;
+    return (jos(n - 1, k)                + k - 1)                    % n                + 1;
+    //     |get val from 0 to n-1|     |to shift & k a diff|      |total n terms|    |to getting overall values of 1 to n|
+}
